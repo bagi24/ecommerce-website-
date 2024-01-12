8a1b7ecdea2c6fe84c4b64d61c7c959a8071a9b1
unknown <bagrat.injgia24@gmail.com>
2024-01-12 21:07:51 +0400

    added Hero component, install react-slick for Slider and added slider functionality and styled

--- src/components/navbar/DarkMode.jsx
+++ src/components/navbar/DarkMode.jsx
@@ -1,22 +1,22 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import LightButton from "../../assets/website/light-mode-button.png";
-import DarkButton from "../../assets/website/dark-mode-button.png";
+import React from 'react';
+import { useState, useEffect } from 'react';
+import LightButton from '../../assets/website/light-mode-button.png';
+import DarkButton from '../../assets/website/dark-mode-button.png';
 
 const DarkMode = () => {
   const [theme, setTheme] = useState(
-    localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
+    localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light',
   );
 
   const element = document.documentElement; // html element
 
   useEffect(() => {
-    if (theme === "dark") {
-      element.classList.add("dark");
-      localStorage.setItem("theme", "dark");
+    if (theme === 'dark') {
+      element.classList.add('dark');
+      localStorage.setItem('theme', 'dark');
     } else {
-      element.classList.remove("dark");
-      localStorage.setItem("theme", "light");
+      element.classList.remove('dark');
+      localStorage.setItem('theme', 'light');
     }
   }, [theme]);
 
@@ -25,15 +25,15 @@ const DarkMode = () => {
       <img
         src={LightButton}
         alt=""
-        onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-        className={`w-12 cursor-pointer drop-shadow-[1px_1px_1px_rgba(0,0,0,0.1)] transition-all duration-300 absolute right-0 z-10 ${
-          theme === "dark" ? "opacity-0" : "opacity-100"
+        onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+        className={`absolute right-0 z-10 w-12 cursor-pointer drop-shadow-[1px_1px_1px_rgba(0,0,0,0.1)] transition-all duration-300 ${
+          theme === 'dark' ? 'opacity-0' : 'opacity-100'
         } `}
       />
       <img
         src={DarkButton}
         alt=""
-        onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+        onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
         className="w-12 cursor-pointer drop-shadow-[1px_1px_1px_rgba(0,0,0,0.1)] transition-all duration-300"
       />
     </div>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,89 +1,89 @@
-import React from "react";
-import Logo from "../../assets/logo.png";
-import { IoMdSearch } from "react-icons/io";
-import { FaCartShopping } from "react-icons/fa6";
-import { FaCaretDown } from "react-icons/fa";
-import DarkMode from "./DarkMode";
+import React from 'react';
+import Logo from '../../assets/logo.png';
+import { IoMdSearch } from 'react-icons/io';
+import { FaCartShopping } from 'react-icons/fa6';
+import { FaCaretDown } from 'react-icons/fa';
+import DarkMode from './DarkMode';
 
 const Menu = [
   {
     id: 1,
-    name: "Home",
-    link: "/#",
+    name: 'Home',
+    link: '/#',
   },
   {
     id: 2,
-    name: "Top Rated",
-    link: "/#services",
+    name: 'Top Rated',
+    link: '/#services',
   },
   {
     id: 3,
-    name: "Kids Wear",
-    link: "/#",
+    name: 'Kids Wear',
+    link: '/#',
   },
   {
     id: 3,
-    name: "Mens Wear",
-    link: "/#",
+    name: 'Mens Wear',
+    link: '/#',
   },
   {
     id: 3,
-    name: "Electronics",
-    link: "/#",
+    name: 'Electronics',
+    link: '/#',
   },
 ];
 
 const DropdownLinks = [
   {
     id: 1,
-    name: "Trending Products",
-    link: "/#",
+    name: 'Trending Products',
+    link: '/#',
   },
   {
     id: 2,
-    name: "Best Selling",
-    link: "/#",
+    name: 'Best Selling',
+    link: '/#',
   },
   {
     id: 3,
-    name: "Top Rated",
-    link: "/#",
+    name: 'Top Rated',
+    link: '/#',
   },
 ];
 
 const Navbar = ({ handleOrderPopup }) => {
   return (
-    <div className="shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40">
+    <div className="relative z-40 bg-white shadow-md duration-200 dark:bg-gray-900 dark:text-white">
       {/* upper Navbar */}
       <div className="bg-primary/40 py-2">
-        <div className="container flex justify-between items-center">
+        <div className="container flex items-center justify-between">
           <div>
-            <a href="#" className="font-bold text-2xl sm:text-3xl flex gap-2">
+            <a href="#" className="flex gap-2 text-2xl font-bold sm:text-3xl">
               <img src={Logo} alt="Logo" className="w-10" />
               Shopsy
             </a>
           </div>
 
           {/* search bar */}
-          <div className="flex justify-between items-center gap-4">
-            <div className="relative group hidden sm:block">
+          <div className="flex items-center justify-between gap-4">
+            <div className="group relative hidden sm:block">
               <input
                 type="text"
                 placeholder="search"
-                className="w-[200px] sm:w-[200px] group-hover:w-[300px] transition-all duration-300 rounded-full border border-gray-300 px-2 py-1 focus:outline-none focus:border-1 focus:border-primary dark:border-gray-500 dark:bg-gray-800  "
+                className="focus:border-1 w-[200px] rounded-full border border-gray-300 px-2 py-1 transition-all duration-300 focus:border-primary focus:outline-none group-hover:w-[300px] dark:border-gray-500 dark:bg-gray-800 sm:w-[200px]  "
               />
-              <IoMdSearch className="text-gray-500 group-hover:text-primary absolute top-1/2 -translate-y-1/2 right-3" />
+              <IoMdSearch className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 group-hover:text-primary" />
             </div>
 
             {/* order button */}
             <button
               onClick={() => handleOrderPopup()}
-              className="bg-gradient-to-r from-primary to-secondary transition-all duration-200 text-white  py-1 px-4 rounded-full flex items-center gap-3 group"
+              className="group flex items-center gap-3 rounded-full bg-gradient-to-r  from-primary to-secondary px-4 py-1 text-white transition-all duration-200"
             >
-              <span className="group-hover:block hidden transition-all duration-200">
+              <span className="hidden transition-all duration-200 group-hover:block">
                 Order
               </span>
-              <FaCartShopping className="text-xl text-white drop-shadow-sm cursor-pointer" />
+              <FaCartShopping className="cursor-pointer text-xl text-white drop-shadow-sm" />
             </button>
 
             {/* Darkmode Switch */}
@@ -95,12 +95,12 @@ const Navbar = ({ handleOrderPopup }) => {
       </div>
       {/* bellow Navbar */}
       <div data-aos="zoom-in" className="flex justify-center">
-        <ul className="sm:flex hidden items-center gap-4">
+        <ul className="hidden items-center gap-4 sm:flex">
           {Menu.map((data) => (
             <li key={data.id}>
               <a
                 href={data.link}
-                className="inline-block px-4 hover:text-primary duration-200"
+                className="inline-block px-4 duration-200 hover:text-primary"
               >
                 {data.name}
               </a>
@@ -114,7 +114,7 @@ const Navbar = ({ handleOrderPopup }) => {
                 <FaCaretDown className="transition-all duration-200 group-hover:rotate-180" />
               </span>
             </a>
-            <div className="absolute z-[9999] hidden group-hover:block w-[200px] rounded-md bg-white p-2 text-black shadow-md">
+            <div className="absolute z-[9999] hidden w-[200px] rounded-md bg-white p-2 text-black shadow-md group-hover:block">
               <ul>
                 {DropdownLinks.map((data) => (
                   <li key={data.id}>
